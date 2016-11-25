1b1483c6065dc288269b18cf0dc0347ceab6e746
alexis-turpin <106575@supinfo.com>
2016-11-25 23:57:32 +0800

    Add get_settings from S3 remote state in api
    get the database config from the terraform remote state JSON in the S3
    buckets.
    Works locally
    #TODO: must be tested in an EC2 with the right IAM role (S3 read)

--- api/api.py
+++ api/api.py
@@ -3,6 +3,9 @@ from tornado.ioloop import IOLoop
 import allBananasHandler
 import homeHandler
 import singleBananaHandler
+import requests
+import boto3
+import json
 
 
 class Application(tornado.web.Application):
@@ -14,15 +17,32 @@ class Application(tornado.web.Application):
         ]
         tornado.web.Application.__init__(self, handlers, autoreload=True)
 
+
 def get_settings():
-    # TODO : Get the DB_USER / PASSWD / API_DNS_NAME / DB_NAME
-        # from the terraform remote state on S3 (boto3)
-    return {
-        "db_user": "root",
-        "db_passwd": "Password01",
-        "db_endpoint": "localhost",
-        "db_name": "bananadb",
-    }
+    try:
+        sg = requests.get("http://169.254.169.254/latest/meta-data/security-groups").text
+        infra = sg.split("-")[0]
+    except Exception as e:
+        print("No AWS infrastructure detected, localhost configuration loaded\n", str(e))
+        return {
+            "db_user": "root",
+            "db_passwd": "Password01",
+            "db_endpoint": "localhost",
+            "db_name": "bananadb",
+        }
+    else:
+        # Get the database config file from S3
+        s3 = boto3.client("s3")
+        file = s3.get_object(Bucket="terraform-states-iac-experiment", Key=infra+"/data.tfstate")
+        file_js = json.loads(file["Body"].read().decode("utf-8"))
+        db_conf = file_js["modules"][1]["resources"]["aws_db_instance.main"]["primary"]["attributes"]
+        return {
+            "db_user": db_conf["username"],
+            "db_passwd": db_conf["password"],
+            "db_endpoint": db_conf["address"],
+            "db_name": db_conf["name"],
+        }
+        # TODO : Test from EC2 with right IAM role (S3 read)
 
 
 def main():
@@ -32,5 +52,5 @@ def main():
     IOLoop.current().start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
